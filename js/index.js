c235be84cb0d020d8c2b7c90c9f7ecdd9c47c268
mihaelkyeah <mihaelkyeah@gmail.com>
2023-03-01 16:31:43 -0300

    Updated index.html. Added utils.css with general purpose classes (to reuse)

--- js/index.js
+++ js/index.js
@@ -8,4 +8,18 @@ window.addEventListener('load', () => {
         list_item.innerHTML = instrument;
         document.querySelector('#real_perf-available_ins').appendChild(list_item);
     });
+
+    let radio_labels = document.querySelectorAll('.pushable-label');
+    Array.prototype.forEach.call(radio_labels, (label) => {
+        label.addEventListener('click', () => {
+            let radios = document.querySelectorAll('.getstarted-radio');
+            Array.prototype.forEach.call(radios, (radio) => {
+                if(label.getAttribute('for') === radio.value)
+                    radio.checked = true;
+                else
+                    radio.checked = false;
+            });
+            document.querySelector('#getstarted-ordernow_button').href = label.getAttribute('for') + "-tracks.html";
+        });
+    });
 });
